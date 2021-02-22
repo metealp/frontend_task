76acab47f813432a5d250327b1ed922274f02f45
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2021-02-22 23:16:15 +0300

    created entrypoint

--- main.py
+++ main.py
@@ -2,34 +2,25 @@ import flask
 import pandas as pd
 
 
-def create_app():
-    app = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
-        """
-        Index page view handler.
-        :return: rendered index.html template
-        """
-        return flask.render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """
+    Index page view handler.
+    :return: rendered index.html template
+    """
+    return flask.render_template('index.html')
 
-    @app.route('/data', methods=['GET', 'POST'])
-    def data():
-        """
-        Data view handler
-        :return: JSON object of the data CSV file
-        """
-        data = pd.read_csv('task_data.csv')
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    """
+    Data view handler
+    :return: JSON object of the data CSV file
+    """
+    data = pd.read_csv('task_data.csv')
 
-        context = {
-            'sensor_data': data.to_dict(orient='list')
-        }
-        return flask.jsonify(context)
-
-    return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    # serve the application on port 7410
-    app.run()
+    context = {
+        'sensor_data': data.to_dict(orient='list')
+    }
+    return flask.jsonify(context)
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from main import app 
+
+if __name__ == "__main__":
+    # app = create_app()
+    # serve the application on port 7410
+    app.run()

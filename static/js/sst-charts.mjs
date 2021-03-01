57998f1d1b2f51517eaa744342f78dbc38fdb01c
metealp <kizilcaymetealp@gmail.com>
2021-03-02 00:21:44 +0300

    add grid for sensors

--- static/js/sst-charts.mjs
+++ static/js/sst-charts.mjs
@@ -26,6 +26,22 @@ fetch('/data')
             chart.xAxis[0].setExtremes(len-11,len-1);
             chart.showResetZoom();
         })
+
+        //SENSOR STATISTICS
+        let element = document.getElementById(`${sensor}-statistics`)
+        element.innerHTML = `
+        <h5>Statistics</h5>
+        <table><thead><tr><th>Argument</th><th>Total</th><th>Positive</th><th>Negative</th></tr></thead>
+        <tbody>
+            <tr><td>Mean</td><td>${sensorData.totalMean.toFixed(3)}</td><td>${sensorData.positiveMean.toFixed(3)}</td><td>${sensorData.negativeMean.toFixed(3)}</td></tr>
+            <tr><td>Variance</td><td>${sensorData.totalVariance.toFixed(3)}</td><td>${sensorData.positiveVariance.toFixed(3)}</td><td>${sensorData.negativeVariance.toFixed(3)}</td></tr>
+            <tr><td>Std</td><td>${sensorData.totalStd.toFixed(3)}</td><td>${sensorData.positiveStd.toFixed(3)}</td><td>${sensorData.negativeStd.toFixed(3)}</td></tr>
+            <tr><td>Median</td><td>${sensorData.totalMedian.toFixed(3)}</td><td>${sensorData.positiveMedian.toFixed(3)}</td><td>${sensorData.negativeMedian.toFixed(3)}</td></tr>
+            <tr><td>Min</td><td>${sensorData.totalMin.toFixed(3)}</td><td>${sensorData.positiveMin.toFixed(3)}</td><td>${sensorData.negativeMin.toFixed(3)}</td></tr>
+            <tr><td>Max</td><td>${sensorData.totalMax.toFixed(3)}</td><td>${sensorData.positiveMax.toFixed(3)}</td><td>${sensorData.negativeMax.toFixed(3)}</td></tr>
+        </tbody>
+        </table>
+        `
         
         //POLAR DISTRIBUTION CHART
         const polarizedData = sensorData.negativeDistribution.map(elm => elm * -1)
@@ -57,11 +73,9 @@ fetch('/data')
         Highcharts.chart(`${sensor}-total-distribution-chart`, totalDistributionChartOptions([{name: "total", data: sensorData.totalDistribution}]))
     }
 
-    console.log(masterSerie)
     const masterReadingsChartOptions = allSensorsReadingChartOptions(fetched.sample_index, masterSerie);
     Highcharts.chart(`master`, masterReadingsChartOptions, function(chart) {
         const len = fetched.class_label.length;
-        console.log(len)
         chart.xAxis[0].setExtremes(len-11,len-1);
         chart.showResetZoom();
     });

0888df57c5dbad305cfe2258379463acfeb7e95a
metealp <kizilcaymetealp@gmail.com>
2021-03-04 10:57:58 +0300

    delete loggers

--- static/js/Rail.js
+++ static/js/Rail.js
@@ -1,15 +1,12 @@
 const Scroll = {
     scrollToElement: function(elemId){
         const elem = document.querySelector(`#${elemId}`);
-        console.log(elemId)
-        console.log(elem)
         elem.scrollIntoView();
     },
 
     setActive: function(closestTitleId){
         const previousActiveTitle = document.querySelector(".active.item");
         previousActiveTitle.classList.remove('active');
-
         const nextActiveTitle = document.querySelector(`#${closestTitleId}-link`);
         nextActiveTitle.classList.add('active');
     },
@@ -28,7 +25,6 @@ const Scroll = {
             // https://gomakethings.com/detecting-when-a-visitor-has-stopped-scrolling-with-vanilla-javascript/
             window.clearTimeout( isScrolling );
             isScrolling = setTimeout(function() {
-                // Run the callback
                 let yDifference = 0;
                 let closestTitleId = "intro-container";
                 const currentY = document.documentElement.scrollTop;
